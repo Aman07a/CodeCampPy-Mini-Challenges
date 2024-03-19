75373832f5013dc641b899fc9adc4eee6a9fb893
Aman07a <aman16.aa17@gmail.com>
2024-03-19 10:20:09 +0100

    Loops - Part 2

--- lesson08/loops.py
+++ lesson08/loops.py
@@ -1,5 +1,5 @@
-# value = 1
-# while value < 10:
+value = 1
+# while value <= 10:
 #     print(value)
 #     if value == 5:
 #         break
@@ -13,11 +13,16 @@
 # else:
 #     print("Value is now equal to " + str(value))
 
-# names = ["Dave", "Sara", "John"]
+names = ["Dave", "Sara", "John"]
 # for x in names:
 #     print(x)
 
-# for x in "Mississipi":
+# for x in "Mississippi":
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         break
 #     print(x)
 
 # for x in names:
@@ -25,5 +30,24 @@
 #         continue
 #     print(x)
 
-for x in range(5):
+# for x in range(4):
+#     print(x)
+
+# for x in range(2, 4):
+#     print(x)
+
+for x in range(5, 101, 5):
     print(x)
+else:
+    print("Glad that's over!")
+
+names = ["Dave", "Sara", "John"]
+actions = ["codes", "eats", "sleeps"]
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action + ".")
+
+for action in actions:
+    for name in names:
+        print(name + " " + action + ".")
