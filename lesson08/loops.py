5978ff2cb22ecbd4a7179131f06dd2fd832dccd3
Aman07a <aman16.aa17@gmail.com>
2024-03-19 09:06:13 +0100

    Loops -Part 1

---
+++ lesson08/loops.py
@@ -0,0 +1,29 @@
+# value = 1
+# while value < 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("Value is now equal to " + str(value))
+
+# names = ["Dave", "Sara", "John"]
+# for x in names:
+#     print(x)
+
+# for x in "Mississipi":
+#     print(x)
+
+# for x in names:
+#     if x == "Sara":
+#         continue
+#     print(x)
+
+for x in range(5):
+    print(x)
